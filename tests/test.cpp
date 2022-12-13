ff27d16f9ca24baca1e8f1b30eea3198a8ab1ed1
Xyphuz <hansen0715.2@gmail.com>
2022-12-13 23:20:27 +0800

    chore: add test of baum_welch

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,5 @@
 #include "viterbi.hpp"
+#include "baum_welch.hpp"
 #include "gtest/gtest.h"
 
 double emission_probability(double emit_value, BiasState state) {
@@ -54,6 +55,58 @@ TEST(tests, viterbi_scalar) {
 
     vector<BiasState> expected_result = { DownstreamBias, UpstreamBias, DownstreamBias, UpstreamBias, DownstreamBias, UpstreamBias, NoBias };
     EXPECT_EQ(viterbi_result, expected_result);
+}
+
+TEST(tests, baum_welch_scalar) {
+    // Define the HMM parameters
+    // A:  transition matrix
+    std::vector<std::vector<double>> A = {{0.7, 0.2, 0.1},
+                                          {0.1, 0.6, 0.3},
+                                          {0.2, 0.3, 0.5}};
+    
+    // B:  emission matrix
+    std::vector<std::vector<double>> B = {{0.7, 0.2, 0.1},
+                                          {0.1, 0.6, 0.3},
+                                          {0.2, 0.1, 0.6}};
+
+    // pi: initial state distribution
+    std::vector<double> pi = {0.4, 0.3, 0.3};
+
+    std::transform(pi.begin(), pi.end(), pi.begin(), [](auto& p){return std::log1p(p);});
+    for (std::size_t i = 0; i < A.size(); ++i) {
+        std::transform(A[i].begin(), A[i].end(), A[i].begin(), [](auto& a){return std::log1p(a);});
+        std::transform(B[i].begin(), B[i].end(), B[i].begin(), [](auto& b){return std::log1p(b);});
+    }
+
+    // Define the observed sequence
+    std::vector<int> O = {0, 1, 2, 2, 1, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1, 0};
+
+    // Estimate the HMM parameters using the Baum-Welch algorithm
+    baum_welch(O);
+
+    std::vector<std::vector<double>> A_expected = {{0.530628, 0.182321, 0.095310},
+                                                   {0.095310, 0.470003, 0.262364},
+                                                   {0.182321, 0.262364, 0.405465}};
+    std::vector<std::vector<double>> B_expected = {{0.530628, 0.182321, 0.095310},
+                                                   {0.095310, 0.470003, 0.262364},
+                                                   {0.182321, 0.095310, 0.470003}};
+    std::vector<double> pi_expected = {0.336472, 0.262364, 0.262364};
+
+    for (std::size_t i = 0; i < A.size(); ++i) {
+        for (std::size_t j = 0; j < A[i].size(); ++j) {
+            EXPECT_NEAR(A[i][j], A_expected[i][j], 1e-6);
+        }
+    }
+
+    for (std::size_t i = 0; i < B.size(); ++i) {
+        for (std::size_t j = 0; j < B[i].size(); ++j) {
+            EXPECT_NEAR(B[i][j], B_expected[i][j], 1e-6);
+        }
+    }
+
+    for (std::size_t i = 0; i < pi.size(); ++i) {
+        EXPECT_NEAR(pi[i], pi_expected[i], 1e-6);
+    }
 }
 
 int main(int argc, char** argv) {
