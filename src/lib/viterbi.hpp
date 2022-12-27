9a66400a55d681dc0db15973cfd9a3afe76bcc47
Xyphuz <hansen0715.2@gmail.com>
2022-12-27 23:24:32 +0800

    fix: using log to avoid prob multiplying and causing disappearance

--- src/lib/viterbi.hpp
+++ src/lib/viterbi.hpp
@@ -11,52 +11,50 @@ float emission_func(const float* const& emission, const int& di, const int& stat
 
 int* viterbi(const int* const& observations, const std::size_t& num_observation, const float* const& initial, const float* const& transition, const float* const& emission) {
     int* hidden_states = new int[num_observation]();
-    float* viterbi = new float[num_observation * 3]();
+    float* viterbi = new float[3* num_observation]();
 
     // initialize viterbi
     for (std::size_t i = 0; i < 3; ++i) {
-        viterbi[i] = initial[i] * emission_func(emission, observations[0], i);
+        viterbi[i * num_observation] = std::log1p(initial[i]) + std::log1p(emission_func(emission, observations[0], i));
     }
 
-    // run viterbi for t > 0
+    // run viterbi
     for (std::size_t t = 1; t < num_observation; ++t) {
         for (std::size_t i = 0; i < 3; ++i) {
-            float max_prob = 0;
+            float max = -INFINITY;
             for (std::size_t j = 0; j < 3; ++j) {
-                float prob = viterbi[(t - 1) * 3 + j] * transition[j * 3 + i];
-                if (prob > max_prob) {
-                    max_prob = prob;
+                float temp = viterbi[j * num_observation + t - 1] + std::log1p(transition[j * 3 + i]);
+                if (temp > max) {
+                    max = temp;
                 }
             }
-            viterbi[t * 3 + i] = max_prob * emission_func(emission, observations[t], i);
+            viterbi[i * num_observation + t] = max + std::log1p(emission_func(emission, observations[t], i));
         }
     }
 
-    // find the most probable state and its backtrack
-    float max_prob = 0;
-    int max_prob_index = 0;
+    // find the most probable state
+    float max = -INFINITY;
     for (std::size_t i = 0; i < 3; ++i) {
-        if (viterbi[(num_observation - 1) * 3 + i] > max_prob) {
-            max_prob = viterbi[(num_observation - 1) * 3 + i];
-            max_prob_index = i;
+        if (viterbi[i * num_observation + num_observation - 1] > max) {
+            max = viterbi[i * num_observation + num_observation - 1];
+            hidden_states[num_observation - 1] = i;
         }
     }
 
-    hidden_states[num_observation - 1] = max_prob_index;
-    for (int t = num_observation - 2; t >= 0; --t) {
-        float max_prob = 0;
-        int max_prob_index = 0;
-        for (std::size_t i = 0; i < 3; ++i) {
-            float prob = viterbi[t * 3 + i] * transition[i * 3 + hidden_states[t + 1]];
-            if (prob > max_prob) {
-                max_prob = prob;
-                max_prob_index = i;
+    // backtrace
+    for (int i = num_observation - 2; i >= 0; --i) {
+        float max = -INFINITY;
+        for (std::size_t j = 0; j < 3; ++j) {
+            float temp = viterbi[j * num_observation + i] + std::log1p(transition[hidden_states[i + 1] * 3 + j]);
+            if (temp > max) {
+                max = temp;
+                hidden_states[i] = j;
             }
         }
-        hidden_states[t] = max_prob_index;
     }
 
     delete[] viterbi;
+
     return hidden_states;
 }
 }
